30d1fc459c91f1861be58c7714782e7a2bd36655
galle <galle@192.168.0.16>
2015-11-19 19:25:27 +0100

    Metodo "numeroIntentos" eliminado e introducido directamente dentro del metodo "comparar"

--- src/bol/pkg10/pkg2/Calculos.java
+++ src/bol/pkg10/pkg2/Calculos.java
@@ -11,15 +11,9 @@ public class Calculos {
     int numeroAleatorio = (int) (Math.random()*50);
     int numeroIntentos;
 
-
-    public int introduccionIntentos() {
-        numeroIntentos = Integer.parseInt(JOptionPane.showInputDialog("Introduce numero de intentos"));
-        return numeroIntentos;
-    }
-
     public void comparar() {
 
-        numeroIntentos = introduccionIntentos();
+        numeroIntentos = Integer.parseInt(JOptionPane.showInputDialog("Introduce numero de intentos"));
         for (int i = 0; i < numeroIntentos; i++) {
             int numRespuesta = Integer.parseInt(JOptionPane.showInputDialog("Adivina un numero entre el 1 y el 50"));
             if (numRespuesta > (numeroAleatorio+20)|numRespuesta < (numeroAleatorio-20)) {
