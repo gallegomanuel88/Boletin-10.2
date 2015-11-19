07738c28fde8198a8be53f1d9d6a0f840210bd7a
galle <galle@192.168.0.16>
2015-11-19 19:12:49 +0100

    Ejercicio finalizados

---
+++ src/bol/pkg10/pkg2/Bol102.java
@@ -0,0 +1,20 @@
+package bol.pkg10.pkg2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author galle
+ */
+public class Bol102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculos obj = new Calculos ();
+        obj.comparar();
+        JOptionPane.showMessageDialog(null, "GameOver");
+    }
+    
+}
