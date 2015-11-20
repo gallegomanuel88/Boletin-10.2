273bbf51b627904b43193bf6f21337e430a5855d
galle <galle@192.168.0.14>
2015-11-21 00:48:55 +0100

    INtroduccion metodo "introduccionIntento" con un "do while" para verificar que el numero de intentos sea positivo.

--- src/bol/pkg10/pkg2/Calculos.java
+++ src/bol/pkg10/pkg2/Calculos.java
@@ -10,10 +10,17 @@ public class Calculos {
     
     int numeroAleatorio = (int) (Math.random()*50);
     int numeroIntentos;
+    
+    public int introduccionIntentos (){
+        do {
+            numeroIntentos = Integer.parseInt(JOptionPane.showInputDialog("Jugador 1: Introduce numero de intentos para el jugador 2"));
+        } while (numeroIntentos < 1);
+        return numeroIntentos;
+    }
 
     public void comparar() {
 
-        numeroIntentos = Integer.parseInt(JOptionPane.showInputDialog("Introduce numero de intentos"));
+        numeroIntentos = introduccionIntentos ();
         for (int i = 0; i < numeroIntentos; i++) {
             int numRespuesta = Integer.parseInt(JOptionPane.showInputDialog("Adivina un numero entre el 1 y el 50"));
             if (numRespuesta > (numeroAleatorio+20)|numRespuesta < (numeroAleatorio-20)) {
